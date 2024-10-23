81a405d2bc31800d65438a4c0205dbf69929dd47
raouf-z <121855203+raouf-z@users.noreply.github.com>
2024-10-23 13:45:03 +0100

    update flutter version

--- code/frontend/lib/main.dart
+++ code/frontend/lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 // to connect with backend
 import 'package:http/http.dart' as http;
-// firts page 
+// firts page
 import './screens//terms_page.dart';
 
 void main() {
-  runApp(
-    const MyApp());
-    
-
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,27 +16,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-    //   home: Scaffold(
-    //     appBar: AppBar(
-    //       title: const Text(' TESTING '),
-    //     ),
-    //   body: Center(
-    //     child: ElevatedButton(
-    //       onPressed: () async {
-    //         final response = await http.get(Uri.parse('http://192.168.201.70:3000/api/data'));
-    //         print('Response: ${response.body}');
-    //       },
-    //       child: const Text('connected to node.js'),
-    //       ),
-    //     ),
-    //   ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(' TESTING '),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              final response = await http
+                  .get(Uri.parse('http://192.168.201.70:3000/api/data'));
+              print('Response: ${response.body}');
+            },
+            child: const Text('connected to node.js'),
+          ),
+        ),
+      ),
 
-      title: 'Your App Title',
-      initialRoute: '/terms', // Set the initial route to the terms page
-      routes: {
-        '/terms': (context) => const TermsPage(), // Route to the TermsPage
-      },
-
+      // title: 'Your App Title',
+      // initialRoute: '/terms', // Set the initial route to the terms page
+      // routes: {
+      //   '/terms': (context) => const TermsPage(), // Route to the TermsPage
+      // },
     );
   }
 }
